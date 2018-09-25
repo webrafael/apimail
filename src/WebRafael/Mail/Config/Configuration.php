fca218703fd859adf200578ec393f391f3673e69
Rafael ANdrade <web.andrade@hotmail.com>
2018-09-25 17:25:56 -0300

    modification into Configuration

--- src/WebRafael/Mail/Config/Configuration.php
+++ src/WebRafael/Mail/Config/Configuration.php
@@ -6,6 +6,28 @@ use Symfony\Component\Dotenv\Dotenv;
 
 class Configuration
 {
+    
+    /**
+     * $path
+     *
+     * @var string
+     * @access public
+     */
+    static public $path;
+
+    /**
+     * getDir()
+     *
+     * @return void
+     * @access protected
+     */
+    static protected function getDir()
+    {
+        $file       = file_exists(dirname(__DIR__, 6).'/.env');
+        self::$path = (!is_null(self::$path)) ? self::$path : $file;
+        return self::$path;
+    }
+
     /**
      * get
      * 
@@ -16,7 +38,7 @@ class Configuration
     static public function get(string $name = null)
     {
         $config = new Dotenv();
-        $config->load(dirname(__DIR__, 6).'/.env');
+        $config->load(self::getDir());
 
         return getenv($name);
     }
